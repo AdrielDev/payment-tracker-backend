1339635e66e32bd58bf56164e3c5abe4a3d4fa58
Adriel Almeida <adriel.almeida@philips.com>
2023-11-26 03:47:28 -0300

    feat: [PT-2] added dto response to user

---
+++ src/main/java/com/api/paymenttracke/dto/user/UserResponseDTO.java
@@ -0,0 +1,49 @@
+package com.api.paymenttracke.dto.user;
+
+import java.time.LocalDate;
+
+public class UserResponseDTO {
+    private Long id;
+    private String cpf;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private LocalDate birthDate;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
